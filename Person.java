74902b17196f54b22d15eba4c609a552151e7b4d
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-11 11:29:49 +0530

    [Madhav] Add: Making copy Constructor

---
+++ Person.java
@@ -0,0 +1,29 @@
+class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy Constructor
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    // Display method
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Madhav", 22);
+        Person p2 = new Person(p1);
+
+        p1.display();
+        p2.display();
+    }
+}
